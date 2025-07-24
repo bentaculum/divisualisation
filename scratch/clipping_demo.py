02304be386934bc3bbd17108851c938bcb240836
Martin Weigert <marweigert@gmail.com>
2025-07-24 11:05:23 -0400

    adding clipping plane example

---
+++ scratch/clipping_demo.py
@@ -0,0 +1,37 @@
+import napari
+import numpy as np
+
+
+if __name__ == "__main__":
+    # Create sample volume
+    x = np.random.randint(0, 100, (100, 100, 100))
+
+    x_broadcast = np.broadcast_to(x[None, ...], (len(x), )+x.shape )
+    
+    
+    viewer = napari.Viewer()
+    layer = viewer.add_image(x_broadcast, rendering='mip', name='volume')
+
+
+
+    viewer.dims.ndisplay = 3
+
+    # Update clipping plane based on time (axis 0)
+    def update_clipping_plane(event=None):
+        t = viewer.dims.point[0]
+        # Move clipping plane along Z axis to t (or adjust axis as needed)
+        layer.experimental_clipping_planes = [{
+            'position': (t, 0, 0),
+            'normal': (1, 0, 0),
+            'enabled': True,
+        },
+            {
+            'position': (t+1, 0, 0),
+            'normal': (-1, 0, 0),
+            'enabled': True,
+        }]
+
+    # Connect event
+    viewer.dims.events.point.connect(update_clipping_plane)
+
+    
