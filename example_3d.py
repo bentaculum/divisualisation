58935419d8425e61b8822664c9e5ba566a3fc1f6
Benjamin Gallusser <bgallusser@googlemail.com>
2025-07-29 23:31:22 -0700

    Add errors on 3d

--- example_3d.py
+++ example_3d.py
@@ -1,59 +1,86 @@
 import logging
+import os
 import pickle
 import pprint
+import urllib
+import urllib.request
+import zipfile
 from pathlib import Path
 
 import napari
-
-# from trackastra.tracking import load_ctc_graph, graph_to_napari_tracks
+import numpy as np
+from tqdm import tqdm
 from traccuracy import run_metrics
 from traccuracy.loaders import load_ctc_data
 from traccuracy.matchers import CTCMatcher
 from traccuracy.metrics import CTCMetrics
 
 from divisualisation import Divisualisation
-from divisualisation.utils import (
-    load_tiff_timeseries,
-)
+from divisualisation.utils import load_tiff_timeseries, rescale_intensity
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 pp = pprint.PrettyPrinter(indent=4)
 
-# gt = load_ctc_data(
-#     "/Users/bgallusser/data/ctc/Fluo-C3DL-MDA231/01_GT/TRA",
-#     "/Users/bgallusser/data/ctc/Fluo-C3DL-MDA231/01_GT/TRA/man_track.txt",
-#     name="gt",
-# )
-# pred = load_ctc_data(
-#     "/Users/bgallusser/data/ctc/Fluo-C3DL-MDA231/01_RES_trackastra",
-#     "/Users/bgallusser/data/ctc/Fluo-C3DL-MDA231/01_RES_trackastra/man_track.txt",
-#     name="res",
-# )
+
+class DownloadProgressBar(tqdm):
+    def update_to(self, b=1, bsize=1, tsize=None):
+        if tsize is not None:
+            self.total = tsize
+        self.update(b * bsize - self.n)
+
+
 if "gt" not in locals():
+    url = "http://data.celltrackingchallenge.net/training-datasets/Fluo-N3DH-CE.zip"
+    data_dir = "data/celegans/downloads/"
+
+    if not os.path.exists(data_dir):
+        os.mkdir(data_dir)
+
+    filename = url.split("/")[-1]
+    file_path = os.path.join(data_dir, filename)
+    ds_name = filename.split(".")[0]
+    if not os.path.exists(file_path):
+        print(f"Downloading {ds_name} data from the CTC website")
+        # Downloading data
+        with DownloadProgressBar(
+            unit="B", unit_scale=True, miniters=1, desc=url.split("/")[-1]
+        ) as t:
+            urllib.request.urlretrieve(url, file_path, reporthook=t.update_to)
+        # Unzip the data
+        with zipfile.ZipFile(file_path, "r") as zip_ref:
+            zip_ref.extractall(data_dir)
+
+    gt_path = Path("data/celegans/TRA")
+    if not os.path.exists(gt_path):
+        with zipfile.ZipFile(f"{gt_path}.zip", "r") as zip_ref:
+            zip_ref.extractall(gt_path.parent)
     gt = load_ctc_data(
-        "/Users/bgallusser/code/traccuracy/downloads/Fluo-N3DH-CE/01_GT/TRA",
-        "/Users/bgallusser/code/traccuracy/downloads/Fluo-N3DH-CE/01_GT/TRA/man_track.txt",
-        run_checks=False,
-        name="gt",
-    )
-    pred = load_ctc_data(
-        "/Users/bgallusser/code/traccuracy/downloads/Fluo-N3DH-CE/01_GT/TRA",
-        "/Users/bgallusser/code/traccuracy/downloads/Fluo-N3DH-CE/01_GT/TRA/man_track.txt",
+        str(gt_path),
+        str(gt_path / "man_track.txt"),
         run_checks=False,
         name="gt",
     )
 
-    img = load_tiff_timeseries(
-        Path("/Users/bgallusser/code/traccuracy/downloads/Fluo-N3DH-CE/01")
+    pred_path = Path("data/celegans/RES")
+    if not os.path.exists(pred_path):
+        with zipfile.ZipFile(f"{pred_path}.zip", "r") as zip_ref:
+            zip_ref.extractall(pred_path.parent)
+    pred = load_ctc_data(
+        str(pred_path),
+        str(pred_path / "man_track.txt"),
+        run_checks=False,
+        name="trackatra_prediction",
     )
-    # img = np.stack(
-    #     [
-    #         rescale_intensity(_x, pmin=1, pmax=99.8, clip=False, subsample=16)
-    #         for _x in tqdm(img, desc="Rescale intensity")
-    #     ]
-    # )
+
+    img = load_tiff_timeseries(Path("data/celegans/downloads/Fluo-N3DH-CE/01"))[:195]
+
+    if True:
+        img = np.stack([
+            rescale_intensity(_x, pmin=5, pmax=99.9, clip=False, subsample=16)
+            for _x in tqdm(img, desc="Rescale intensity")
+        ])
 
     matched_path = "3d_matched.pkl"
     try:
@@ -70,7 +97,6 @@ if "gt" not in locals():
 
     gt_graph = ctc_matched.gt_graph
     pred_graph = ctc_matched.pred_graph
-    # networkx graph at traccuracy.TrackingGraph.graph
 
 v = napari.current_viewer()
 if v is not None:
@@ -79,28 +105,28 @@ v = napari.Viewer()
 for layer in v.layers:
     v.layers.remove(layer)
 v.theme = "dark"
-div = Divisualisation(
+
+divis = Divisualisation(
     z_scale=5,
-    time_scale=10,
+    time_scale=5,
 )
 
-v = div.visualize_gt(
+v = divis.visualize_gt(
     v,
     x=img,
-    masks=gt.segmentation,
+    masks=pred.segmentation,
+    # networkx graph at traccuracy.TrackingGraph.graph
     graph=gt_graph.graph,
 )
 
-v.dims.ndisplay = 3
+v = divis.visualize_edge_errors(
+    viewer=v,
+    gt_graph=gt_graph,
+    pred_graph=pred_graph,
+    masks_original=gt.segmentation,
+    masks_tracked=pred.segmentation,
+)
+
+v.dims.set_current_step(0, 190)
 v.camera.angles = (27.919484296382873, -49.86671510905139, -35.8190766165135)
 v.camera.perspective = 27
-
-
-# errors_layer, errors_data = visualize_edge_errors(
-#     viewer=v,
-#     gt_graph=gt_graph,
-#     pred_graph=pred_graph,
-#     masks_original=gt.segmentation,
-#     masks_tracked=pred.segmentation,
-#     scale=scale,
-# )
--- src/divisualisation/divisualisation.py
+++ src/divisualisation/divisualisation.py
@@ -5,13 +5,9 @@ import networkx as nx
 import numpy as np
 import traccuracy
 from napari.utils.colormaps.colormap_utils import vispy_or_mpl_colormap
-
-# from trackastra.tracking import load_ctc_graph, graph_to_napari_tracks
 from traccuracy import EdgeFlag
 
 from .utils import graph_to_napari_tracks
-
-# from visualize_2d_tracking_errors import visualize_edge_errors
 
 logger = logging.getLogger(__name__)
 
@@ -21,16 +17,13 @@ class Divisualisation:
         self,
         time_scale: int = 10,
         z_scale: int | None = 1,
+        tracks_width: int = 1,
     ):
         self.time_scale = time_scale
         self.z_scale = z_scale
         # TODO adapt for 2D. Fake 3rd dimension that does not do anything
         self.scale = (self.time_scale, self.z_scale, 1, 1)
-
-    # def render_error_video(self, viewer, path):
-    #     Animation = napari.Animation()
-    #     self.take_keyframe(viewer)
-    #     self.move_slider(viewer)
+        self.tracks_width = tracks_width
 
     def visualize_gt(
         self,
@@ -38,55 +31,46 @@ class Divisualisation:
         x: np.ndarray,
         masks: np.ndarray,
         graph: nx.DiGraph,
+        time_attr: str = "t",
     ):
         tracks, _, properties = graph_to_napari_tracks(
             graph,
-            properties=["t"],
+            properties=[time_attr],
         )
 
         # TODO figure out broadcasting for 2D case
         # xc_broadcast = np.broadcast_to(xc[None, ...], (len(xc),) + xc.shape)
-        img_layer = v.add_image(
+        translate_to_tracks = [
+            0,
+            -self.z_scale * (x.shape[1] / 2) + self.z_scale,
+            0,
+            0,
+        ]
+
+        v.add_image(
             # np.expand_dims(xc, 0),
             x,
             scale=(1, self.z_scale, 1, 1),
-            # translate = [0, -scale[1] * img.shape[1] / 2 + scale[1], 0, 0]
             colormap="gray",
             rendering="mip",
+            translate=translate_to_tracks,
             # depiction="plane",
         )
 
-        labels_layer = v.add_labels(
-            # np.expand_dims(mc, 0),
+        v.add_labels(
             masks,
             name="masks",
             scale=(1, self.z_scale, 1, 1),
             # rendering="iso_categorical",
             rendering="translucent",
-            opacity=1.0,
+            translate=translate_to_tracks,
+            opacity=0.2,
             # depiction="plane",
         )
 
-        # TODO remove hardcoded dep on specific property
-        properties["gt"] = np.ones_like(properties["t"]) * 0.5
-        # lin = np.array(properties["lineage"])
-
-        # # shuffle lineage ids
-        # rng = np.random.default_rng(0)
-        # perm = rng.permutation(int(lin.max()) + 1)
-        # lin_perm = perm[lin.astype(int)]
-        # properties["lineage_permuted"] = 0.0 + 1.0 * (
-        #     lin_perm.astype(float) / max(lin_perm.max(), 1)
-        # )
-
-        # properties["track_id_normalized"] = 0.3 + 0.7 * (track_ids / track_ids.max())
-        # properties["lineage_normalized"] = lin.astype(float) / lin.max()
+        properties["gt"] = np.ones_like(properties[time_attr]) * 0.5
 
         properties = {"gt": properties["gt"]}
-
-        # tracks[:, 1] = tracks[:, 1] * 2  # Double time points to match image
-        # tracks = np.concat([tracks[:, 0:1], tracks[:, 1:2], tracks[:, 1:]], axis=1)
-
         logger.info("Adding gt tracks")
 
         # Scale tracks z dim by time
@@ -95,31 +79,20 @@ class Divisualisation:
 
         tracks_layer = v.add_tracks(
             data=tracks,
-            # graph=tracks_graph,
             name="tracks",
             properties=properties,
-            # TODO can I just scale adaptively here?
-            # scale=(self.time_scale, 1, 1, 1),
             color_by="gt",
             blending="translucent_no_depth",
             colormaps_dict={
-                # "lineage_permuted": vispy_or_mpl_colormap("Greens"),
-                # "lineage_permuted": label_colormap(seed=0.12),
-                # "lineage_normalized": label_colormap(seed=0.12),
-                # "track_id_normalized": vispy_or_mpl_colormap("gray_r"),
-                # "gt": vispy_or_mpl_colormap("greens"),
-                # For white bg
                 "gt": vispy_or_mpl_colormap("Greens"),
             },
-            # tail_width=2.0,
-            tail_width=2,
+            tail_width=self.tracks_width,
             tail_length=1000,
             opacity=1.0,
         )
 
         def update_gt_state(event=None):
             t = v.dims.point[0]
-            # Move clipping plane along Z axis to t (or adjust axis as needed)
             clipping_planes_tracks = [
                 {
                     "position": (0, 0, 0),
@@ -136,96 +109,109 @@ class Divisualisation:
             tracks_layer.translate = [0, -self.time_scale * (t + 1), 0, 0]
 
         v.dims.events.point.connect(update_gt_state)
-        translate_to_tracks = [
-            0,
-            -self.z_scale * (x.shape[1] / 2) + self.z_scale,
-            0,
-            0,
-        ]
-        img_layer.affine.translate = translate_to_tracks
-        labels_layer.affine.translate = translate_to_tracks
-        v.dims.set_current_step(0, img_layer.data.shape[0])
+        v.dims.ndisplay = 3
+        # v.dims.set_current_step(0, img_layer.data.shape[0])
+        v.camera.center = (
+            -self.time_scale * x.shape[0] / 2,
+            v.camera.center[1],
+            v.camera.center[2],
+        )
+        v.camera.zoom = 0.5
 
         return v
 
-
-def visualize_edge_errors(
-    viewer: napari.Viewer,
-    gt_graph: traccuracy.TrackingGraph,
-    pred_graph: traccuracy.TrackingGraph,
-    masks_original: np.ndarray,
-    masks_tracked: np.ndarray,
-    scale: tuple,
-    feats: str = "",
-    translate: list = [0, 0, 0],
-):
-    errors_layer = {}
-    errors_data = {}
-    for i, (error, graph, reference_masks, cmap) in enumerate(
-        zip(
-            (EdgeFlag.CTC_FALSE_NEG, EdgeFlag.CTC_FALSE_POS),
-            (gt_graph, pred_graph),
-            (masks_original, masks_tracked),
-            # ("hsv", "gray_r"),
-            # ("PiYG", "PiYG"),
-            ("cool", "cool"),
-        )
+    def visualize_edge_errors(
+        self,
+        viewer: napari.Viewer,
+        gt_graph: traccuracy.TrackingGraph,
+        pred_graph: traccuracy.TrackingGraph,
+        masks_original: np.ndarray,
+        masks_tracked: np.ndarray,
     ):
-        np.zeros_like(masks_original)
-        edge_error_tracks = []
-        edge_error_props = {"error_type": []}
-        for edge_id, (u_id, v_id) in enumerate(graph.get_edges_with_flag(error)):
-            # Skip edges that are not in the reference masks
-            # if row.t_u >= len(reference_masks) or row.t_v >= len(reference_masks):
-            # continue
-
-            edge_id += 1  # avoid edge id 0
-            u = graph.nodes[u_id]
-            v = graph.nodes[v_id]
-            # Get positions of the edge endpoints
-            # c_u = np.argwhere(reference_masks[u["t"]] == u["segmentation_id"]).mean(
-            # axis=0
-            # )
-            edge_error_tracks.append([edge_id, u["t"], u["y"], u["x"]])
-            edge_error_props["error_type"].append(i)
-
-            # c_v = np.argwhere(reference_masks[v["t"]] == v["segmentation_id"]).mean(
-            # axis=0
-            # )
-            edge_error_tracks.append([edge_id, v["t"], v["y"], v["x"]])
-            edge_error_props["error_type"].append(i)
-
-        edge_error_props = {k: np.array(v) for k, v in edge_error_props.items()}
-
-        if len(edge_error_tracks) > 0:
-            tracks = np.array(edge_error_tracks)
-            # tracks[:, 1] = tracks[:, 1] * 2  # Double time points to match image
-            tracks = np.concat([tracks[:, 0:1], tracks[:, 1:2], tracks[:, 1:]], axis=1)
-            layer = viewer.add_tracks(
-                data=tracks,
-                properties=edge_error_props,
-                color_by="error_type",
-                # colormap=cmap,
-                colormaps_dict={
-                    "error_type": vispy_or_mpl_colormap(cmap),
-                },
-                scale=scale,
-                # tail_width=8,
-                tail_width=5,
-                head_length=1,
-                tail_length=1000,
-                visible=True,
-                blending="translucent_no_depth",
-                opacity=1.0,
-                translate=translate,
-                name=error,
+        errors_layer = {}
+        # errors_data = {}
+        for i, (error, graph, reference_masks, cmap) in enumerate(
+            zip(
+                (EdgeFlag.CTC_FALSE_NEG, EdgeFlag.CTC_FALSE_POS),
+                (gt_graph, pred_graph),
+                (masks_original, masks_tracked),
+                # ("hsv", "gray_r"),
+                # ("PiYG", "PiYG"),
+                ("cool", "cool"),
             )
-            errors_layer[error] = layer
-            errors_data[error] = {
-                "tracks": tracks,
-                "properties": edge_error_props,
-            }
-        else:
-            logger.info(f"No edge errors of type {error}")
+        ):
+            np.zeros_like(masks_original)
+            edge_error_tracks = []
+            edge_error_props = {"error_type": []}
+            for edge_id, (u_id, v_id) in enumerate(graph.get_edges_with_flag(error)):
+                # Skip edges that are not in the reference masks
+                # if row.t_u >= len(reference_masks) or row.t_v >= len(reference_masks):
+                # continue
 
-    return errors_layer, errors_data
+                edge_id += 1  # avoid edge id 0
+                u = graph.nodes[u_id]
+                v = graph.nodes[v_id]
+                # Get positions of the edge endpoints
+                # c_u = np.argwhere(reference_masks[u["t"]] == u["segmentation_id"]).mean(
+                # axis=0
+                # )
+                edge_error_tracks.append([edge_id, u["t"], u["z"], u["y"], u["x"]])
+                edge_error_props["error_type"].append(i)
+
+                # c_v = np.argwhere(reference_masks[v["t"]] == v["segmentation_id"]).mean(
+                # axis=0
+                # )
+                edge_error_tracks.append([edge_id, v["t"], v["z"], v["y"], v["x"]])
+                edge_error_props["error_type"].append(i)
+
+            edge_error_props = {k: np.array(v) for k, v in edge_error_props.items()}
+
+            if len(edge_error_tracks) > 0:
+                tracks = np.array(edge_error_tracks)
+
+                # Scale tracks z dim by time
+                assert tracks.shape[1] == 5
+                tracks[:, -3] = tracks[:, -3] + self.time_scale * tracks[:, -4]
+                layer = viewer.add_tracks(
+                    data=tracks,
+                    properties=edge_error_props,
+                    color_by="error_type",
+                    # colormap=cmap,
+                    colormaps_dict={
+                        "error_type": vispy_or_mpl_colormap(cmap),
+                    },
+                    tail_width=self.tracks_width * 2,
+                    head_length=1,
+                    tail_length=1000,
+                    visible=True,
+                    blending="translucent_no_depth",
+                    opacity=1.0,
+                    name=error,
+                )
+                errors_layer[error] = layer
+
+            else:
+                logger.info(f"No edge errors of type {error}")
+
+        def update_errors_state(event=None):
+            t = viewer.dims.point[0]
+            clipping_planes_tracks = [
+                {
+                    "position": (0, 0, 0),
+                    "normal": (0, 0, 0),
+                    "enabled": False,
+                },
+                {
+                    "position": ((t + 1) * self.time_scale, 0, 0),
+                    "normal": (-1, 0, 0),
+                    "enabled": True,
+                },
+            ]
+            for error, layer in errors_layer.items():
+                layer.experimental_clipping_planes = clipping_planes_tracks
+                layer.translate = [0, -self.time_scale * (t + 1), 0, 0]
+
+        viewer.dims.events.point.connect(update_errors_state)
+        viewer.dims.ndisplay = 3
+
+        return viewer
