32c6bd56f403ae5bb94746640bacea1b9d8e046d
Benjamin Gallusser <bgallusser@googlemail.com>
2026-07-20 13:48:33 -0700

    Rename plugin widget to 'Lift tracks' (shows as 'Lift tracks (Divisualisation)')

--- example_2d.py
+++ example_2d.py
@@ -84,6 +84,6 @@ for graph, name in ((gt_graph, "GT tracks"), (pred_graph, "predicted tracks")):
 # directly, as the flat functional API.)
 
 # Dock the plugin widgets so you can toggle + play interactively.
-viewer.window.add_plugin_dock_widget("divisualisation", "Spacetime lift")
+viewer.window.add_plugin_dock_widget("divisualisation", "Lift tracks")
 
 napari.run()
